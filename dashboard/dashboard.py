0d6cdaa326476f5650321d79dd332d6f6dc99e6e
adrinta <adrintaabdurrazzaq@gmail.com>
2025-02-27 08:30:58 +0700

    update

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,56 +4,8 @@ import seaborn as sns
 import streamlit as st
 
 sns.set(style='dark')
-
-def get_total_count_by_hour_df(hour_df):
-  hour_count_df =  hour_df.groupby(by="hours").agg({"count_cr": ["sum"]})
-  return hour_count_df
-
-def count_by_day_df(day_df):
-    day_df_count_2011 = day_df.query(str('dteday >= "2011-01-01" and dteday < "2012-12-31"'))
-    return day_df_count_2011
-
-def total_registered_df(day_df):
-   reg_df =  day_df.groupby(by="dteday").agg({
-      "registered": "sum"
-    })
-   reg_df = reg_df.reset_index()
-   reg_df.rename(columns={
-        "registered": "register_sum"
-    }, inplace=True)
-   return reg_df
-
-def total_casual_df(day_df):
-   cas_df =  day_df.groupby(by="dteday").agg({
-      "casual": ["sum"]
-    })
-   cas_df = cas_df.reset_index()
-   cas_df.rename(columns={
-        "casual": "casual_sum"
-    }, inplace=True)
-   return cas_df
-
-def sum_order (hour_df):
-    sum_order_items_df = hour_df.groupby("hours").count_cr.sum().sort_values(ascending=False).reset_index()
-    return sum_order_items_df
-
-def macem_season (day_df): 
-    season_df = day_df.groupby(by="season").count_cr.sum().reset_index() 
-    return season_df
-
-days_df = pd.read_csv("./day_clean.csv")
-hours_df = pd.read_csv("./hour_clean.csv")
-
-datetime_columns = ["dteday"]
-days_df.sort_values(by="dteday", inplace=True)
-days_df.reset_index(inplace=True)   
-
-hours_df.sort_values(by="dteday", inplace=True)
-hours_df.reset_index(inplace=True)
-
-for column in datetime_columns:
-    days_df[column] = pd.to_datetime(days_df[column])
-    hours_df[column] = pd.to_datetime(hours_df[column])
+days_df = pd.read_csv("./df_day_clean.csv")
+hours_df = pd.read_csv("./df_hour_clean.csv")
 
 min_date_days = days_df["dteday"].min()
 max_date_days = days_df["dteday"].max()
@@ -61,110 +13,105 @@ max_date_days = days_df["dteday"].max()
 min_date_hour = hours_df["dteday"].min()
 max_date_hour = hours_df["dteday"].max()
 
-with st.sidebar:
-    # Menambahkan logo perusahaan
-    st.image("https://storage.googleapis.com/gweb-uniblog-publish-prod/original_images/image1_hH9B4gs.jpg")
-    
-        # Mengambil start_date & end_date dari date_input
+
+st.markdown("<h1 style='text-align:center;text-decoration:underline'>Bike Sharing Dashboard<h1>", unsafe_allow_html=True)
+
+with st.expander("Filter"):
     start_date, end_date = st.date_input(
-        label='Rentang Waktu',
-        min_value=min_date_days,
-        max_value=max_date_days,
-        value=[min_date_days, max_date_days])
-  
-main_df_days = days_df[(days_df["dteday"] >= str(start_date)) & 
-                       (days_df["dteday"] <= str(end_date))]
+                label="Tanggal",
+                min_value=min_date_days,
+                max_value=max_date_days,
+                value=[min_date_days, max_date_days])
+    
+    season = st.selectbox( label="Musim",
+                          options=('All', 'Springer', 'Summer', 'Fall', 'Winter'))
+    
+    temperature = st.selectbox(label="Suhu",
+                               options=('All', 'Sangat Rendah', 'Rendah', 'Sedang', 'Tinggi', 'Sangat Tinggi'))
+    
+    weather = st.selectbox(label="Cuaca",
+                                options=('All',
+                                         'Clear, Few clouds, Partly cloudy, Partly cloudy',
+                                         'Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist',
+                                         'Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds',
+                                         'Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog'))
+    workingday = st.selectbox(label="Hari Kerja",
+                                options=('All','Ya', "Tidak"))
+    
+    hue = st.selectbox(label="Hue",
+                        options=('All', 'season', 'temperature_cat', 'weathersit', 'workingday'))
 
-main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) & 
-                        (hours_df["dteday"] <= str(end_date))]
 
-hour_count_df = get_total_count_by_hour_df(main_df_hour)
-day_df_count_2011 = count_by_day_df(main_df_days)
-reg_df = total_registered_df(main_df_days)
-cas_df = total_casual_df(main_df_days)
-sum_order_items_df = sum_order(main_df_hour)
-season_df = macem_season(main_df_hour)
 
-#Melengkapi Dashboard dengan Berbagai Visualisasi Data
-st.header('Bike Sharing :sparkles:')
+filtered_days_df = days_df[(days_df["dteday"] >= str(start_date)) & (days_df["dteday"] <= str(end_date))]
+filtered_days_df['dteday'] =  pd.to_datetime(filtered_days_df["dteday"])
 
-st.subheader('Daily Sharing')
+if season != 'All':
+    filtered_days_df = filtered_days_df[filtered_days_df['season']==season]
+if temperature != 'All':
+    filtered_days_df = filtered_days_df[filtered_days_df['temperature_cat']==temperature]
+if weather != 'All':
+    filtered_days_df = filtered_days_df[filtered_days_df['weathersit']==weather]
+if workingday != 'All':
+    if workingday == 'Ya':
+        filtered_days_df = filtered_days_df[filtered_days_df['workingday']=='Yes']
+    else:
+        filtered_days_df = filtered_days_df[filtered_days_df['workingday']=='No']
+if hue == 'All':
+    hue = None
+
 col1, col2, col3 = st.columns(3)
- 
+
 with col1:
-    total_orders = day_df_count_2011.count_cr.sum()
-    st.metric("Total Sharing Bike", value=total_orders)
+    total_orders = filtered_days_df.cnt.sum()
+    st.metric("Jumlah Penyewa (Semua)", value=total_orders)
 
 with col2:
-    total_sum = reg_df.register_sum.sum()
-    st.metric("Total Registered", value=total_sum)
+    total_sum = filtered_days_df.registered.sum()
+    st.metric("Jumlah Penyewa (Registered)", value=total_sum)
 
 with col3:
-    total_sum = cas_df.casual_sum.sum()
-    st.metric("Total Casual", value=total_sum)
-
-st.subheader("Performa penjualan perusahaan dalam beberapa tahun terakhir")
+    total_sum = filtered_days_df.casual.sum()
+    st.metric("Jumlah Penyewa (Casual)", value=total_sum)
 
 fig, ax = plt.subplots(figsize=(16, 8))
-ax.plot(
-    days_df["dteday"],
-    days_df["count_cr"],
-    marker='o', 
-    linewidth=2,
-    color="#90CAF9"
+sns.scatterplot(
+    filtered_days_df,
+    x='dteday',
+    y='cnt',
+    hue=hue
 )
+ax.set_title('Jumlah Penyewa / Hari', fontsize=30)
+ax.set_xlabel('Tanggal', fontsize=20)
+ax.set_ylabel('Jumlah Penyewa', fontsize=20)
 ax.tick_params(axis='y', labelsize=20)
-ax.tick_params(axis='x', labelsize=15)
+ax.tick_params(axis='x', labelsize=20)
 st.pyplot(fig)
 
-st.subheader("pada jam berapa yang paling banyak dan paling sedikit disewa?")
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+filtered_hours_df = hours_df[(hours_df["dteday"] >= str(start_date)) & (hours_df["dteday"] <= str(end_date))]
+filtered_hours_df['dteday'] =  pd.to_datetime(filtered_hours_df["dteday"])
 
-sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#D3D3D3", "#D3D3D3", "#90CAF9", "#D3D3D3", "#D3D3D3"], ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Hours (PM)", fontsize=30)
-ax[0].set_title("Jam dengan banyak penyewa sepeda", loc="center", fontsize=30)
-ax[0].tick_params(axis='y', labelsize=35)
-ax[0].tick_params(axis='x', labelsize=30)
- 
-sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3","#90CAF9"], ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Hours (AM)",  fontsize=30)
-ax[1].set_title("Jam dengan sedikit penyewa sepeda", loc="center", fontsize=30)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].tick_params(axis='y', labelsize=35)
-ax[1].tick_params(axis='x', labelsize=30)
- 
-st.pyplot(fig)
-st.subheader("musim apa yang paling banyak disewa?")
+if season != 'All':
+    filtered_hours_df = filtered_hours_df[filtered_hours_df['season']==season]
+if temperature != 'All':
+    filtered_hours_df = filtered_hours_df[filtered_hours_df['temperature_cat']==temperature]
+if weather != 'All':
+    filtered_hours_df = filtered_hours_df[filtered_hours_df['weathersit']==weather]
+if workingday != 'All':
+    if workingday == 'Ya':
+        filtered_hours_df = filtered_hours_df[filtered_hours_df['workingday']=='Yes']
+    else:
+        filtered_hours_df = filtered_hours_df[filtered_hours_df['workingday']=='No']
 
-colors = ["#D3D3D3", "#D3D3D3", "#D3D3D3", "#90CAF9"]
-fig, ax = plt.subplots(figsize=(20, 10))
+fig, ax = plt.subplots(figsize=(16, 8))
 sns.barplot(
-        y="count_cr", 
-        x="season",
-        data=season_df.sort_values(by="season", ascending=False),
-        palette=colors,
-        ax=ax
-    )
-ax.set_title("Grafik Antar Musim", loc="center", fontsize=50)
-ax.set_ylabel(None)
-ax.set_xlabel(None)
-ax.tick_params(axis='x', labelsize=35)
-ax.tick_params(axis='y', labelsize=30)
+    filtered_hours_df.groupby('hr').cnt.mean().reset_index(),
+    x='hr',
+    y='cnt'
+)
+ax.set_title('Rata - Rata Penyewa / Hari / Jam', fontsize=20)
+ax.set_xlabel('Jam', fontsize=20)
+ax.set_ylabel('Rata - Rata Penyewa', fontsize=20)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=20)
 st.pyplot(fig)
-
-st.subheader("Perbandingan Customer yang Registered dengan casual")
-
-labels = 'casual', 'registered'
-sizes = [18.8, 81.2]
-explode = (0, 0.1) 
-
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',colors=["#D3D3D3", "#90CAF9"],
-        shadow=True, startangle=90)
-ax1.axis('equal')  
-
-st.pyplot(fig1)
